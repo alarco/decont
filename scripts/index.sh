61fd3619d2f74e6dd587d0a48c64abebf0a8b352
Alicia Arenas <arenasalic@gmail.com>
2019-11-18 20:06:32 +0100

    Commit inicial para index.sh

--- scripts/index.sh
+++ scripts/index.sh
@@ -3,4 +3,4 @@
 
 # The STAR command is provided for you. You should replace the parts surrounded by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir $WD/res/contaminants_idx --genomeFastaFiles $WD/res --genomeSAindexNbases 9
