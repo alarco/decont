e83d899925500af6dae918f4e6f7d0c6aeab8563
Alicia Arenas <arenasalic@gmail.com>
2019-11-18 20:25:29 +0100

    Commit inicial para el archivo merge

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,6 @@
 # This script should merge all files from a given sample (the sample id is provided in the third argument)
 # into a single file, which should be stored in the output directory specified by the second argument.
 # The directory containing the samples is indicated by the first argument.
+
+
+cat file1.tar.gz file2.tar.gz > merged.tar.gz
